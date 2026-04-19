fd60c476d77adb69c838881f0fb7772f722756b9
Srikanthgsr1234 <gangavarapusrikanth2001@gmail.com>
2026-04-19 23:07:50 +0530

    updated app logic

--- app.py
+++ app.py
@@ -6,18 +6,15 @@ import google.generativeai as genai
 load_dotenv()
 genai.configure(api_key=os.getenv('gemini_api_key'))
 st.title('Image application')
-uploaded_file=st.file_uploader('upload a image')
-
+"""Upload an image and enter a prompt to generate content based on the image and the prompt."""
+uploaded_file=st.file_uploader('upload a image',type=['jpg','jpeg','png']  )
 if uploaded_file is not None:
     st.image(Image.open(uploaded_file))
 prompt=st.text_input('Enter your prompt')
-img=Image.open(uploaded_file)
+
 
 if st.button('Generate'):
+    img=Image.open(uploaded_file)
     model=genai.GenerativeModel("gemini-2.5-flash")
     response = model.generate_content([prompt, img])
     st.success(response.text)
-    
-# st.title('Image application')
-# propmt=st.text_input
-# img=Image.open
